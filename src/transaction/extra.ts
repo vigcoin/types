a02332b5fc3880d8107f19e006c6d4cd896c75d7
calidion <calidion@gmail.com>
2021-01-10 01:39:28 +0800

    added transaction types

---
+++ src/transaction/extra.ts
@@ -0,0 +1,31 @@
+import { usize } from "../basic";
+import { IPublicKey } from "../key";
+
+export const TX_EXTRA_PADDING_MAX_COUNT = 255;
+export const TX_EXTRA_NONCE_MAX_COUNT = 255;
+
+export enum ITransactionExtraTag {
+  PADDING = 0x00,
+  PUBKEY = 0x01,
+  NONCE = 0x02,
+}
+
+export interface ITransactionExtraPadding {
+  size: usize;
+}
+
+export interface ITransactionExtraPublicKey {
+  key: IPublicKey;
+}
+
+export interface ITransactionExtraNonce {
+  nonce: Buffer;
+}
+
+export interface ITransactionExtra {
+  tag: ITransactionExtraTag;
+  data:
+    | ITransactionExtraNonce
+    | ITransactionExtraPublicKey
+    | ITransactionExtraPadding;
+}
--- src/transaction/index.ts
+++ src/transaction/index.ts
@@ -1,5 +1,7 @@
-import { uint16, uint32, uint64, uint8, usize } from './basic';
-import { IHash, IKeyImage, IPublicKey, ISignature } from './key';
+export * from "./extra";
+
+import { uint16, uint32, uint64, uint8, usize } from "../basic";
+import { IHash, IKeyImage, IPublicKey, ISignature } from "../key";
 
 // Input Transactions
 export interface IInputBase {
@@ -41,7 +43,7 @@ export interface ITransactionOutput {
 export enum ETransactionIOType {
   BASE = 0xff,
   KEY = 0x02,
-  SIGNATURE = 0x03,
+  SIGNATURE = 0x03
 }
 
 export interface ITransactionInput {
